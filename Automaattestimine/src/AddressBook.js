9d9a6249678699faeeb835fdb876301591c216a6
heikov123 <45162091+heikov123@users.noreply.github.com>
2019-03-07 09:56:28 +0200

    asd

---
+++ Automaattestimine/src/AddressBook.js
@@ -0,0 +1,8 @@
+describe('Address Book' , function() {
+    it('should be able to add a contact', function() {
+        var addressBook = new addressBook(),
+            thisContact = new Contact();
+
+        addressBook.addContact(thisContact);
+    });
+});
