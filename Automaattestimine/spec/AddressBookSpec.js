aca686abda5c1f02b816e7af98d1d6355b4b314c
heikov123 <45162091+heikov123@users.noreply.github.com>
2019-03-14 09:54:04 +0200

    Writing an asynchronous test

--- Automaattestimine/spec/AddressBookSpec.js
+++ Automaattestimine/spec/AddressBookSpec.js
@@ -21,4 +21,12 @@ beforeEach(function() {
 
         expect(addressBook.getContact(0)).not.toBeDefined();
     });
+});
+describe('Async Address Book', function() {
+    it('should grab initial contacts', function() {
+        var addressBook = new AddressBook();
+
+        addressBook.getInitialContacts();
+        expect(addressBook.initialComplete).toBe(true);
+    });
 });  
